7a958258684a9e72304447705987488f7dea7130
vishal <vm938359@gmail.com>
2025-10-10 12:38:09 +0530

    update

---
+++ delete.js
@@ -0,0 +1,12 @@
+// Create an object
+const person = {
+  name: "Vishal",
+  age: 24,
+  city: "Noida"
+};
+
+// Delete a property
+delete person.city;
+
+// Display the updated object
+console.log(person);
