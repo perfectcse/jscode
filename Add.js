67a60891e78ec2e1499ac89378f9fdbc9803ae92
vishal <vm938359@gmail.com>
2025-10-10 12:06:32 +0530

    Create Add.js

---
+++ Add.js
@@ -0,0 +1,14 @@
+// Create an object
+const person = {
+  name: "Vishal",
+  age: 24
+};
+
+// Add a new property
+person.city = "Noida";
+
+// OR using bracket notation
+person["country"] = "India";
+
+// Display the updated object
+console.log(person);
